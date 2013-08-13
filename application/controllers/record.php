2ab9bc95d0430839d149e8464bcd55bd83d7b2ce
pulakroy <pulak@apurbatech.com>
2013-08-13 19:00:34 +0600

    added two methods patient_record_insertion and patient_family_record_insertion

--- application/controllers/record.php
+++ application/controllers/record.php
@@ -1,40 +1,106 @@
-<?php defined('BASEPATH') OR exit('No direct script access allowed');
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Record extends CI_Controller {
 
-	function __construct()
-	{
-		 parent::__construct();
+    function __construct() {
+        parent::__construct();
         $this->load->helper('url');
         $this->load->helper('form');
         $this->load->library("template");
         $this->load->library('form_validation');
-	}
+        $this->load->model('record_model');
+    }
 
-	//redirect if needed, otherwise display the user list
-	function index()
-	{
-		$this->load->view('record/record_home');
+    //redirect if needed, otherwise display the user list
+    function index() {
+        $this->load->view('record/record_home');
+    }
 
-	}
-	
-	function input()
-	{
-		$this->load->model('Record_model');
-		$data = $this->Record_model->general();
-		//$this->load->view('books_input',$data);	
-	}
-  
-	function view_list($var = null)
-	{
-		$this->load->model('Record_model');
-		$data = $this->Record_model->general();
-		
-		if($var == 'personal')
-			$this->template->load("templates/add_record_template", 'record/add_record_personal_details', $data); 
-		else if($var == 'family')
-			$this->template->load("templates/add_record_template", 'record/add_record_family_details', $data);
-		//$this->load->view('record/add_record', $data);
-	}
+    function input() {
+        $this->load->model('Record_model');
+        $data = $this->Record_model->general();
+        //$this->load->view('books_input',$data);	
+    }
+
+    function view_list($var = null) {
+        $this->load->model('Record_model');
+        $data = $this->Record_model->general();
+
+        if ($var == 'personal')
+            $this->template->load("templates/add_record_template", 'record/add_record_personal_details', $data);
+        else if ($var == 'family')
+            $this->template->load("templates/add_record_template", 'record/add_record_family_details', $data);
+        //$this->load->view('record/add_record', $data);
+    }
+
+    function patient_record_insertion() {
+        //print_r($this->input->post());
+        //$data = array();
+        //validate form input
+        $this->form_validation->set_rules('fullname', 'Full name', 'required|xss_clean');
+        if ($this->form_validation->run() == true) {
+
+
+            $data = array(
+                'fullname' => $this->input->post('fullname'),
+                'surname' => $this->input->post('surname'),
+                'maiden_name' => $this->input->post('maiden_name'),
+                'ic_no' => $this->input->post('IC_no'),
+                'nationality' => $this->input->post('nationality'),
+                'd_o_b' => $this->input->post('d_o_b'),
+                'padigree_labelling' => $this->input->post('padigree_labelling'),
+                'gender' => $this->input->post('gender'),
+                'ethnicity' => $this->input->post('ethnicity'),
+                'blood_group' => $this->input->post('blood_group'),
+                'comment' => $this->input->post('comment'),
+                'hospital_no' => $this->input->post('comment')
+            );
+
+            //array_push($data, $this->input->post('firstname'));
+            $id = $this->record_model->insert_patient_record($data);
+            if ($id > 0) {
+                echo "Data Added successfully";
+            } else {
+                echo "Failed to insert";
+            }
+        } else {
+            print_r(validation_errors());
+        }
+    }
+    
+    function patient_family_record_insertion()
+    {
+        //print_r($this->input->post());
+        //$data = array();
+        //validate form input
+        $this->form_validation->set_rules('mother_fullname', 'Full name', 'required|xss_clean');
+        if ($this->form_validation->run() == true) {
+
+
+            $data = array(
+                'full_name' => $this->input->post('mother_fullname'),
+                'sur_name' => $this->input->post('mother_surname'),
+                'maiden_name' => $this->input->post('mother_maiden_name'),
+                'patient_ic_no' => $this->input->post('mother_IC_no'),
+                'nationality' => $this->input->post('mother_nationality'),
+                'd_o_b' => $this->input->post('mother_DOB'),
+                'relatives_id' => $this->input->post('mother_IC_no'),
+                'family_no' => $this->input->post('mother_IC_no'),  
+                'cancer_type_id' => $this->input->post('mother_IC_no')   
+            );
+
+            //array_push($data, $this->input->post('firstname'));
+            $id = $this->record_model->insert_patient_family_record($data);
+            if ($id > 0) {
+                echo "Data Added successfully";
+            } else {
+                echo "Failed to insert";
+            }
+        } else {
+            print_r(validation_errors());
+        }
+    }
 
 }
