5d00dd3daaaf51e1a6f4752a3f25cd06976d81fc
pulakroy <pulak@apurbatech.com>
2013-08-14 12:46:41 +0600

    added logout functionaliies link

--- application/views/record/record_home.php
+++ application/views/record/record_home.php
@@ -33,23 +33,42 @@
     <body>
 
         <!-- /navigation bar starts here -->
-		<div class="container">
-			<div class="row" style="padding-top:20px;">
-				<div class="row">
-					<a href="<?php echo base_url(); ?>"><img src="<?php echo base_url(); ?>img/Carif_logo.jpg" alt="logo" width="100%"/></a>
-				</div>
-				<div id="header" class="row">
-					<table border="0" width="60%"><tr><td><a class="btn btn-large btn-info home_btn" href="<?php echo base_url(); ?>">Home</a></td><td><span class="welcome_msg">Welcome to CARIF database system</span></td><td><a class="btn btn-large btn-info logout_btn" href="templates/home.html">Logout</a></td><tr></table>
-				</div>
-				<div>
-					<a class="add_record_tab_btn" href="<?php echo site_url('record/view_list/personal');?>" target="iframe_record_home" >Personal</a><a class="add_record_tab_btn" href="<?php echo site_url('record/view_list/family');?>" target="iframe_record_home" >Family</a>
-					<iframe src="<?php echo site_url('record/view_list/personal');?>" name="iframe_record_home" width="100%" height="700px" scrolling="no"></iframe>
-				</div>
-				<div id="footer" class="row">
-					<p>Copyright. @2013. Carif.</p>
-				</div>
-			</div>
-		</div>
-		<div height="40px"> &nbsp;</div>
+        <div class="container">
+            <div class="row" style="padding-top:20px;">
+                <div class="row">
+                    <a href="<?php echo base_url(); ?>"><img src="<?php echo base_url(); ?>img/Carif_logo.jpg" alt="logo" width="100%"/></a>
+                </div>
+                <div id="header" class="row">
+                    <table border="0" width="60%">
+                        <tr>
+                            <td>
+                                <a class="btn btn-large btn-info home_btn" href="<?php echo base_url(); ?>">Home</a>
+                            </td>
+                            <td>
+                                <span class="welcome_msg">Welcome to CARIF database system</span>
+                            </td>
+                            <td>
+                                <a class="btn btn-large btn-info logout_btn" href="<?php echo site_url('auth/logout'); ?>">
+                                    Logout
+                                </a>
+                            </td>
+                        <tr>
+                    </table>
+                </div>
+                <div>
+                    <a class="add_record_tab_btn" href="<?php echo site_url('record/view_list/personal'); ?>" target="iframe_record_home" >
+                        Personal
+                    </a>
+                    <a class="add_record_tab_btn" href="<?php echo site_url('record/view_list/family'); ?>" target="iframe_record_home" >
+                        Family
+                    </a>
+                    <iframe src="<?php echo site_url('record/view_list/personal'); ?>" name="iframe_record_home" width="100%" height="700px" scrolling="no"></iframe>
+                </div>
+                <div id="footer" class="row">
+                    <p>Copyright. @2013. Carif.</p>
+                </div>
+            </div>
+        </div>
+        <div height="40px"> &nbsp;</div>
     </body>
 </html>
