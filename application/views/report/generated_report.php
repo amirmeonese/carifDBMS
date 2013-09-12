6742df8ff792c1ef72f6c69c9e7f1fd0d6afb25b
pulakroy <pulak@apurbatech.com>
2013-09-12 12:56:13 +0600

    added design to show data according to search key

--- application/views/report/generated_report.php
+++ application/views/report/generated_report.php
@@ -1,26 +1,27 @@
 <div class="container" id="report_div">
     <div id="report_header" class="row">
-        <p>Report Manager</p>
+        <p>Search Result</p>
     </div>
-    <?php echo form_open('report/process_report'); ?>
-    <div class="container" id="report_generation_section">
+    <div class="container" id="report_form_section" >
         <div height="30px">&nbsp;</div>
-        <table>
-            <tr>
-                <td id="label1">
-                    Full Name 
-                </td>
-                <td id="label1">
-                    Date of Birth
-                </td>
-            </tr>
-           <?php echo $result['fullname'] ?>
+        <table border="1" width="50%" style="margin-left:180px;">
+            <thead>
+                <tr>
+                    <th style="background-color:Crimson;">Full Name</th>
+                    <th style="background-color:Crimson;">Sur Name</th>
+                    <th style="background-color:Crimson;">IC</th>
+                </tr>
+            </thead>
+            <?php foreach ($searched_result as $list): ?>
+                <tr>
+                    <td><?php echo $list['fullname']; ?></td>
+                    <td><?php echo $list['surname']; ?></td>
+                    <td><?php echo $list['ic_no']; ?></td>
+                </tr>
+            <?php endforeach; ?>
         </table>
+        </br>
+        <a style="margin-left:180px;" class="submitCancel" href="<?php echo site_url('report/index');?>">Done</a>
     </div>
-
-    <?php echo form_close(); ?>
 </div>
-
-
-
 
