79a8ac99dfba91fe1a4ead79ffd33c2b6269463f
famir <farizaamir@apurbatech.com>
2013-09-27 16:20:22 +0800

    New version of carifdbms, changed fields in Add Record, based on client's recent feedback.

---
+++ application/views/record/add_record_mutation_analysis_details.php
@@ -0,0 +1,180 @@
+<div class="container" id="add_record_div">
+    <div id="add_record_header" class="row">
+        <p>Add Investigation Details</p>
+    </div>
+    <?php echo form_open_multipart("record/investigation_insertion"); ?>
+    <div class="container" id="add_record_form_mutation_analysis_div_1">
+        <div height="30px">&nbsp;</div>
+		<table>
+		<tr> 
+			<td>
+                    <?php echo $IC_no; ?>: 
+                    <?php echo form_input('IC_no'); ?>
+			</td>
+			<td>
+				<?php echo $studies_name; ?>: 
+				<?php echo form_dropdown('studies_name', $studies_name_lists); ?>
+				<?php echo '<br/>'; ?>
+			</td>
+		</tr>
+		</table>
+		<div height="30px">&nbsp;</div>
+        <?php
+        echo form_fieldset('Mutation Analysis');
+        ?>
+        <table id="mutation_section_1">
+			<tr>
+                <td id="label1">Analysis 1</td>
+				<td><input type="button" value="Add mutation analysis" onClick="window.parent.addMutationAnalysis('add_record_form_mutation_analysis_div_1');
+                            window.parent.calcHeight();"></td>
+            </tr>
+            <tr>
+                <td>
+                    <?php echo $date_test_ordered; ?>: <br />
+                    <?php echo form_input('date_test_ordered'); ?>
+                </td>
+                <td>
+                    <?php echo $test_ordered_by; ?>:  <br />
+                    <?php echo form_input('test_ordered_by'); ?>
+                </td>
+                <td>
+                    <?php echo $testing_results_notification_flag; ?>:  
+                    <?php echo form_checkbox('testing_results_notification_flag', '1', FALSE); ?>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <?php echo $investigation_project_name; ?>:  <br />
+                    <?php echo form_dropdown('investigation_project_name', $investigation_project_name_lists); ?>
+                </td>
+                <td>
+                    <?php echo $investigation_project_batch; ?>:  <br />
+                    <?php echo form_input('investigation_project_batch'); ?>
+                </td>
+				<td>
+                    <?php echo $investigation_project_date; ?>:  <br />
+                    <?php echo form_input('investigation_project_date'); ?>
+                </td>
+               <td>
+                    <?php echo $investigation_gene_tested; ?>:  <br />
+                    <?php echo form_dropdown('investigation_gene_tested', $investigation_gene_tested_lists); ?>
+                </td>
+            </tr>
+            <tr>
+				<td>
+                    <?php echo $investigation_gene_tested_other; ?>:  <br />
+                    <?php echo form_input('investigation_gene_tested_other'); ?>
+                </td>
+				<td>
+                    <?php echo $investigation_test_type; ?>:  <br />
+                    <?php echo form_dropdown('investigation_test_type', $investigation_test_type_lists); ?>
+                </td>
+                <td>
+                    <?php echo $investigation_sample_type; ?>:  <br />
+                    <?php echo form_dropdown('investigation_sample_type', $investigation_sample_type_lists); ?>
+                </td>
+            </tr>
+			<tr>
+				<td>
+                    <?php echo $investigation_test_reason; ?>:  <br />
+                    <?php
+                    $data = array(
+                        'name' => 'investigation_test_reason',
+                        'id' => 'investigation_test_reason',
+                        'rows' => '3',
+                        'cols' => '7'
+                    );
+                    echo form_textarea($data);
+                    ?>
+                </td>
+                <td>
+                    <?php echo $investigation_new_mutation_flag; ?>:  
+                    <?php echo form_checkbox('investigation_new_mutation_flag', '1', FALSE); ?>
+                </td>
+			</tr>
+            <tr>
+                <td>
+                    <?php echo $investigation_test_results; ?>:  <br />
+                    <?php echo form_dropdown('investigation_test_results', $investigation_test_results_lists); ?>
+                </td>
+                <td>
+                    <?php echo $investigation_test_results_other_details; ?>:  <br />
+                    <?php
+                    $data = array(
+                        'name' => 'investigation_test_results_other_details',
+                        'id' => 'investigation_test_results_other_details',
+                        'rows' => '3',
+                        'cols' => '7'
+                    );
+                    echo form_textarea($data);
+                    ?>
+                </td>
+                <td>&nbsp;</td>
+            </tr>
+            <tr>
+				 <td>
+                    <?php echo $investigation_mutation_pathogenicity; ?>:  <br />
+                    <?php echo form_dropdown('investigation_mutation_pathogenicity', $investigation_mutation_pathogenicity_lists); ?>
+                </td>
+				 <td>
+                    <?php echo $investigation_mutation_nomenclature; ?>:  <br />
+                    <?php echo form_dropdown('investigation_mutation_nomenclature', $investigation_mutation_nomenclature_lists); ?>
+                </td>
+				 <td>
+                    <?php echo $investigation_mutation_type; ?>: </br>
+                    <?php echo form_input('investigation_mutation_type'); ?>
+                </td>
+				 <td>
+                    <?php echo $investigation_exon; ?>: </br>
+                    <?php echo form_input('investigation_exon'); ?>
+                </td>
+            </tr>
+            <tr>
+				<td>
+                    <?php echo $investigation_carrier_status; ?>:  <br />
+                    <?php echo form_dropdown('investigation_carrier_status', $investigation_carrier_status_lists); ?>
+                </td>
+				 <td>
+                    <?php echo $investigation_report_date; ?>: </br>
+                    <?php echo form_input('investigation_report_date'); ?>
+                </td>
+				<td>
+                    <?php echo $investigation_date_notified; ?>:  <br />
+                    <?php echo form_input('investigation_date_notified'); ?>
+                </td>
+				<td>
+                    <?php echo $mutation_is_counselling_flag; ?>:  
+                    <?php echo form_checkbox('mutation_is_counselling_flag', '1', FALSE); ?>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <?php echo $investigation_test_comment; ?>:  <br />
+                    <?php
+                    $data = array(
+                        'name' => 'investigation_test_comment',
+                        'id' => 'investigation_test_comment',
+                        'rows' => '3',
+                        'cols' => '7'
+                    );
+                    echo form_textarea($data);
+                    ?>
+                </td>
+                <td>
+                    <?php echo $investigation_conformation_attachment; ?>: 	
+                    <?php echo form_checkbox('investigation_conformation_attachment', '1', FALSE); ?>
+                </td>
+                <td>
+                    <input type="file" name="userfile" size="100000" />
+                </td>
+            </tr>
+        </table>
+        <?php echo form_fieldset_close(); ?>
+    </div>
+    <?php echo form_submit('mysubmit', 'Save'); ?>
+    <?php echo form_close(); ?>
+</div>
+
+
+
+
---
+++ application/views/record/add_record_pathology_details.php
@@ -0,0 +1,263 @@
+<div class="container" id="add_record_div">
+    <div id="add_record_header" class="row">
+        <p>Add Pathology</p>
+    </div>
+    <?php echo form_open_multipart("record/surveillance_insertion"); ?>
+    <div class="container" id="add_record_form_section_breast_cancer_pathology_section_1">
+        <div height="30px">&nbsp;</div>
+		<table>
+		<tr> 
+			<td>
+                    <?php echo $IC_no; ?>: 
+                    <?php echo form_input('IC_no'); ?>
+			</td>
+		</tr>
+		</table>
+		<div height="30px">&nbsp;</div>
+        <?php
+        echo form_fieldset('Pathology');
+        ?>
+        <table id="breast_cancer_pathology_section">
+			<tr>
+                <td id="label1">Breast Cancer Tumor Pathology</td>
+				<td><input type="button" value="Add pathology" onClick="window.parent.addBreastCancerPathology('add_record_form_section_breast_cancer_pathology_section_1');
+                            window.parent.calcHeight();"></td>
+            </tr>
+            <tr>
+                <td>
+                    <?php echo $breast_pathology_tissue_site; ?>: <br />
+                     <?php echo form_dropdown('breast_pathology_tissue_site', $patient_cancer_site_lists); ?>
+                </td>
+				<td>
+                    <?php echo $breast_pathology_path_report_type; ?>: <br />
+                    <?php echo form_dropdown('breast_pathology_path_report_type', $pathology_path_report_type_lists); ?>
+                </td>
+				 <td>
+                    <?php echo $breast_pathology_path_report_date; ?>: <br />
+                    <?php echo form_input('breast_pathology_path_report_date'); ?>
+                </td>
+            </tr>
+			<tr>
+				 <td>
+                    <?php echo $breast_pathology_lab; ?>: <br />
+                    <?php echo form_input('breast_pathology_lab'); ?>
+                </td>
+				<td>
+                    <?php echo $breast_pathology_doctor; ?>: <br />
+                    <?php echo form_input('breast_pathology_doctor'); ?>
+                </td>
+			</tr>
+            <tr>
+				<td>
+                    <?php echo $breast_pathology_morphology; ?>: <br />
+                    <?php echo form_dropdown('breast_pathology_morphology', $pathology_morphology_lists); ?>
+                </td>
+				 <td>
+                    <?php echo $breast_pathology_tissue_tumour_stage; ?>: <br />
+                    <?php echo form_dropdown('breast_pathology_tissue_tumour_stage', $pathology_tissue_tumour_stage_lists); ?>
+                </td>
+                <td>
+                    <?php echo $breast_pathology_node_stage; ?>: <br />
+                    <?php echo form_dropdown('breast_pathology_node_stage', $pathology_node_stage_lists); ?>
+                </td>
+            </tr>
+			<tr>
+				 <td>
+                    <?php echo $breast_pathology_metastasis_stage; ?>: <br />
+                    <?php echo form_dropdown('breast_pathology_metastasis_stage', $pathology_metastasis_stage_lists); ?>
+                </td>
+				 <td>
+                    <?php echo $breast_pathology_tumour_stage; ?>: <br />
+                    <?php echo form_dropdown('breast_pathology_tumour_stage', $pathology_tumour_stage_lists); ?>
+                </td>
+				 <td>
+                    <?php echo $breast_pathology_tumour_grade; ?>: <br />
+                    <?php echo form_dropdown('breast_pathology_tumour_grade', $pathology_tumour_grade_lists); ?>
+                </td>
+			</tr>
+			<tr>
+				<td>
+                    <?php echo $breast_pathology_total_lymph_nodes; ?>: <br />
+                    <?php echo form_input('breast_pathology_total_lymph_nodes'); ?>
+                </td>
+				 <td>
+                    <?php echo $breast_pathology_tumour_size; ?>: <br />
+                    <?php echo form_input('breast_pathology_tumour_size'); ?>
+                </td>
+			</tr>
+		</table>
+		<div id="breast_cancer_staining_status_div_1">
+			<table id="breast_cancer_staining_status_section_1">
+				<tr>
+					<td>
+						<?php echo $breast_pathology_ER_status; ?>: <br />
+						<?php echo form_input('breast_pathology_ER_status'); ?>
+					</td>
+					<td>
+						<?php echo $breast_pathology_PR_status; ?>: <br />
+						<?php echo form_input('breast_pathology_PR_status'); ?>
+					</td>
+					<td>
+						<?php echo $breast_pathology_HER2_status; ?>: <br />
+						<?php echo form_input('breast_pathology_HER2_status'); ?>
+					</td>
+					 <td>
+						<input type="button" value="Add staining status" onClick="window.parent.addBreastCancerStainingStatusInput('breast_cancer_staining_status_div_1');
+								window.parent.calcHeight();">
+					</td>
+				</tr>
+			</table>
+		</div>
+		<table>
+			<tr>
+				<td>
+					<?php echo $breast_pathology_tissue_path_comments; ?>: <br />
+					<?php
+					$data = array(
+						'name' => 'breast_pathology_tissue_path_comments',
+						'id' => 'breast_pathology_tissue_path_comments',
+						'rows' => '3',
+						'cols' => '7'
+					);
+					echo form_textarea($data);
+					?>
+				</td>
+				<td>&nbsp;</td>
+				<td>&nbsp;</td>
+			</tr>
+		</table>
+        <?php echo form_fieldset_close(); ?>	
+    </div>
+	<div class="container" id="add_record_form_section_ovary_cancer_pathology_section_1">
+        <div height="30px">&nbsp;</div>
+        <table id="ovary_cancer_pathology_section">
+			<tr>
+                <td id="label1">Ovarian Cancer Tumor Pathology</td>
+				<td><input type="button" value="Add pathology" onClick="window.parent.addOvaryCancerPathology('add_record_form_section_ovary_cancer_pathology_section_1');
+                            window.parent.calcHeight();"></td>
+            </tr>
+            <tr>
+                <td>
+                    <?php echo $ovary_pathology_tissue_site; ?>: <br />
+                     <?php echo form_dropdown('ovary_pathology_tissue_site', $patient_cancer_site_lists); ?>
+                </td>
+				<td>
+                    <?php echo $ovary_pathology_path_report_type; ?>: <br />
+                    <?php echo form_dropdown('ovary_pathology_path_report_type', $pathology_path_report_type_lists); ?>
+                </td>
+				 <td>
+                    <?php echo $ovary_pathology_path_report_date; ?>: <br />
+                    <?php echo form_input('ovary_pathology_path_report_date'); ?>
+                </td>
+            </tr>
+			<tr>
+				 <td>
+                    <?php echo $ovary_pathology_lab; ?>: <br />
+                    <?php echo form_input('ovary_pathology_lab'); ?>
+                </td>
+				<td>
+                    <?php echo $ovary_pathology_doctor; ?>: <br />
+                    <?php echo form_input('ovary_pathology_doctor'); ?>
+                </td>
+			</tr>
+            <tr>
+				<td>
+                    <?php echo $ovary_pathology_morphology; ?>: <br />
+                    <?php echo form_dropdown('ovary_pathology_morphology', $pathology_morphology_lists); ?>
+                </td>
+				<td>
+                    <?php echo $ovary_stage_classification; ?>: <br />
+                    <?php echo form_dropdown('ovary_stage_classification', $ovary_stage_classification_lists); ?>
+                </td>
+				 <td>
+                    <?php echo $ovary_pathology_tumour_stage; ?>: <br />
+                    <?php echo form_dropdown('ovary_pathology_tumour_stage', $pathology_tumour_stage_lists); ?>
+                </td>
+            </tr>
+			<tr>
+				<td>
+					<?php echo $ovary_pathology_tumour_grade; ?>: <br />
+					<?php echo form_dropdown('ovary_pathology_tumour_grade', $pathology_tumour_grade_lists); ?>
+				</td>
+				<td>
+					<?php echo $ovary_pathology_tumour_size; ?>: <br />
+					<?php echo form_input('ovary_pathology_tumour_size'); ?>
+				</td>
+			</tr>
+			<tr>
+				<td>
+					<?php echo $ovary_pathology_tissue_path_comments; ?>: <br />
+					<?php
+					$data = array(
+						'name' => 'ovary_pathology_tissue_path_comments',
+						'id' => 'ovary_pathology_tissue_path_comments',
+						'rows' => '3',
+						'cols' => '7'
+					);
+					echo form_textarea($data);
+					?>
+				</td>
+				<td>&nbsp;</td>
+				<td>&nbsp;</td>
+			</tr>
+		</table>
+        <?php echo form_fieldset_close(); ?>	
+    </div>
+	<div class="container" id="add_record_form_section_other_cancer_pathology_section_1">
+        <div height="30px">&nbsp;</div>
+        <table id="other_cancer_pathology_section">
+			<tr>
+                <td id="label1">Other Cancer Tumor Pathology</td>
+				<td><input type="button" value="Add pathology" onClick="window.parent.addOtherCancerPathology('add_record_form_section_other_cancer_pathology_section_1');
+                            window.parent.calcHeight();"></td>
+            </tr>
+            <tr>
+                <td>
+                    <?php echo $other_pathology_tissue_site; ?>: <br />
+                     <?php echo form_dropdown('other_pathology_tissue_site', $patient_cancer_site_lists); ?>
+                </td>
+				<td>
+                    <?php echo $other_pathology_path_report_type; ?>: <br />
+                    <?php echo form_dropdown('other_pathology_path_report_type', $pathology_path_report_type_lists); ?>
+                </td>
+				 <td>
+                    <?php echo $other_pathology_path_report_date; ?>: <br />
+                    <?php echo form_input('other_pathology_path_report_date'); ?>
+                </td>
+            </tr>
+			<tr>
+				 <td>
+                    <?php echo $other_pathology_lab; ?>: <br />
+                    <?php echo form_input('other_pathology_lab'); ?>
+                </td>
+				<td>
+                    <?php echo $other_pathology_doctor; ?>: <br />
+                    <?php echo form_input('other_pathology_doctor'); ?>
+                </td>
+			</tr>
+			<tr>
+				<td>
+					<?php echo $other_pathology_tissue_path_comments; ?>: <br />
+					<?php
+					$data = array(
+						'name' => 'other_pathology_tissue_path_comments',
+						'id' => 'other_pathology_tissue_path_comments',
+						'rows' => '3',
+						'cols' => '7'
+					);
+					echo form_textarea($data);
+					?>
+				</td>
+				<td>&nbsp;</td>
+				<td>&nbsp;</td>
+			</tr>
+		</table>
+        <?php echo form_fieldset_close(); ?>	
+    </div>
+	<?php echo form_submit('mysubmit', 'Save'); ?>
+    <?php echo form_close(); ?>
+</div>
+
+
+
+
