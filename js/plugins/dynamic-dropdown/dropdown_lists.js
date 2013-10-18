87e8065345690dee1e0442b21369899daa6f8d2c
famir <farizaamir@apurbatech.com>
2013-10-18 18:52:53 +0800

    Added a plugin for form customization module

---
+++ js/plugins/dynamic-dropdown/dropdown_lists.js
@@ -0,0 +1,161 @@
+$( document ).ready(function() {
+	
+	$.getJSON("http://localhost/carifDBMS/js/plugins/dynamic-dropdown/data.json", function(data) 
+	{	
+		//var key = $dropdown.val();
+		var vals = [];
+							
+		// switch(key) {
+			// case 'beverages':
+				// vals = data.beverages.split(",");
+				// break;
+			// case 'snacks':
+				// vals = data.snacks.split(",");
+				// break;
+			// case 'base':
+				// vals = ['Please choose from above'];
+		// }
+		
+		vals = data.nationalities.split(",");
+		var $secondChoice = $("#nationality");
+		$secondChoice.empty();
+		$.each(vals, function(index, value) {
+			$secondChoice.append("<option>" + value + "</option>");
+		});
+		
+		vals = data.gender.split(",");
+		$secondChoice = $("#gender");
+		$secondChoice.empty();
+		$.each(vals, function(index, value) {
+			$secondChoice.append("<option>" + value + "</option>");
+		});
+	});
+	
+	//== EDIT DROPDOWN LISTS==//
+	$( ".dialog-form" ).dialog({
+      autoOpen: false,
+      height: 200,
+      width: 350,
+      modal: true,
+      buttons: {
+        "Save changes": function() {
+			var passedID = $(this).data('id');
+			var selectedValue = $('#' + passedID).val();
+			var newItemName = rename_item_name.value;
+			
+			$.getJSON("http://localhost/carifDBMS/js/plugins/dynamic-dropdown/data.json", function(data) 
+			{	
+				var vals = [];
+				
+				switch(passedID) {
+					case 'nationality':
+					{
+						vals = data.nationalities.split(",");
+						
+						$.each(vals, function(index, value) {
+							if(value == selectedValue)
+								vals[index] = newItemName;
+						});
+						
+						var appendedText = '';
+						$.each(vals, function(index, value) {
+						
+							appendedText = appendedText + value + ',';
+						});
+						
+						//Call PHP to modify JSON file
+						$.ajax({ url: 'http://localhost/carifDBMS/js/plugins/dynamic-dropdown/process.php',
+							 data: {action: 'rename',id:passedID, text: appendedText},
+							 type: 'post',
+							 success: function(output) {
+								alert('Changes saved!');
+								//Redraw dropdown
+								var $secondChoice = $("#nationality");
+								$secondChoice.empty();
+								$.each(vals, function(index, value) {
+									$secondChoice.append("<option>" + value + "</option>");
+								});
+						
+							  }
+						});
+						break;
+					}
+					case 'gender':
+					{
+						vals = data.gender.split(",");
+						
+						$.each(vals, function(index, value) {
+							if(value == selectedValue)
+								vals[index] = newItemName;
+						});
+						
+						var appendedText = '';
+						$.each(vals, function(index, value) {
+						
+							appendedText = appendedText + value + ',';
+						});
+						
+						$.ajax({ url: 'http://localhost/carifDBMS/js/plugins/dynamic-dropdown/process.php',
+							 data: {action: 'rename',id:passedID, text: appendedText},
+							 type: 'post',
+							 success: function(output) {
+								 alert('Changes saved!');
+								 //Redraw dropdown
+								var $secondChoice = $("#gender");
+								$secondChoice.empty();
+								$.each(vals, function(index, value) {
+									$secondChoice.append("<option>" + value + "</option>");
+								});
+							  }
+						});
+						break;
+					}
+				}
+				
+			});
+
+			$( this ).dialog( "close" );
+        },
+        Cancel: function() {
+          $( this ).dialog( "close" );
+        }
+      },
+      close: function() {
+        
+      }
+    });
+	
+	$( ".dialog-form-add-new" ).dialog({
+      autoOpen: false,
+      height: 200,
+      width: 350,
+      modal: true,
+      buttons: {
+        "Save changes": function() {
+			$( this ).dialog( "close" );
+        },
+        Cancel: function() {
+          $( this ).dialog( "close" );
+        }
+      },
+      close: function() {
+       
+      }
+    });
+
+	$( ".rename" ).button().click(function() {
+		var buttonID = this.id;
+		var selectedValue = $('#' + buttonID).val();
+		$("#rename_item_name").val(selectedValue);
+		$( ".dialog-form" ).data('id', buttonID).dialog( "open" );
+	});
+
+	$( ".add-new" ).button().click(function() {
+		$( ".dialog-form-add-new" ).dialog( "open" );
+	});
+	
+	$( ".delete" ).button().click(function() {
+		var buttonID = this.id;
+		var selectedValue = $('#' + buttonID).val();
+	});
+});
---
+++ js/plugins/dynamic-dropdown/process.php
@@ -0,0 +1,46 @@
+<?php
+	function renameItem($id, $text) {
+        
+		//Trim last comma
+		$text = rtrim($text, ",");
+		
+		// Loading existing data:
+		$json = file_get_contents("data.json");
+		$data = json_decode($json, true);
+		
+		switch($id){
+			case 'nationality':
+			{
+				$data['nationalities'] = $text;
+				break;
+			}
+			case 'gender':
+			{
+				$data['gender'] = $text;
+				break;
+			}
+		}
+		
+		// Writing modified data:
+		file_put_contents('data.json', json_encode($data, JSON_FORCE_OBJECT));
+
+		return true;
+    }
+	
+	if(isset($_POST['action']) && !empty($_POST['action'])) {
+		$action = $_POST['action'];
+		$id = $_POST['id'];
+		$text = $_POST['text'];
+		
+		switch($action) {
+			case 'rename' : 
+			{
+				renameItem($id, $text);
+				break;
+			}
+			case 'add' : addItem();break;
+		}	
+	}
+
+	
+?>
