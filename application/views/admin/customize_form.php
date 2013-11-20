684044bc1282a14655851f3e2370eab2a519be2b
famir <farizaamir@apurbatech.com>
2013-11-20 16:46:53 +0800

    Group the dropdown menu items in admin panel and added a data file backup.

--- application/views/admin/customize_form.php
+++ application/views/admin/customize_form.php
@@ -28,29 +28,40 @@
 				<td colspan="2">
 					<select id="first-choice">
 					<option selected value="base">Please Select</option>
-					<option value="nationality">Nationality</option>
-					<option value="gender">Gender</option>
-					<option value="marital_status">Marital Status</option>
-					<option value="COGS_study_id">COGS Study ID</option>
-					<option value="income_level">Income Level</option>
-					<option value="status_source">Status Source</option>
-					<option value="alive_status">Alive Status</option>
-					<option value="studies_name">Studies Names</option>
-					<option value="patient_cancer_name_lists">Cancer Names</option>
-					<option value="cancer_site">Cancer Sites</option>
-					<option value="cancer_invasive_type">Cancer Invasive Types</option>
-					<option value="detected_by">Detected By</option>
-					<option value="pathology_path_report_type_lists">Pathology Type of Report</option>
-					<option value="pathology_morphology_lists">Morphology</option>
-					<option value="pathology_tissue_tumour_stage_lists">T Staging</option>
-					<option value="pathology_node_stage_lists">N Staging</option>
-					<option value="pathology_metastasis_stage_lists">M Staging</option>
-					<option value="pathology_tumour_stage_lists">Tumour Stage</option>
-					<option value="pathology_tumour_grade_lists">Tumour Grade</option>
-					<option value="patient_cancer_treatment_name_lists">Treatment Type</option>
-					<option value="ovary_stage_classification_lists">Pathology Stage Classification</option>
-					<option value="diagnosis_name_lists">Type of Diseases</option>
-					
+					<optgroup label="General">
+						<option value="studies_name">Studies Names</option>
+					</optgroup>
+					<optgroup label="Personal">
+						<option value="nationality">Nationality</option>
+						<option value="gender">Gender</option>
+						<option value="marital_status">Marital Status</option>
+						<option value="COGS_study_id">COGS Study ID</option>
+						<option value="income_level">Income Level</option>
+						<option value="status_source">Status Source</option>
+						<option value="alive_status">Alive Status</option>
+					</optgroup>
+					<optgroup label="Diagnosis">
+						<option value="patient_cancer_name_lists">Cancer Names</option>
+						<option value="cancer_site">Cancer Sites</option>
+						<option value="cancer_invasive_type">Cancer Invasive Types</option>
+						<option value="detected_by">Detected By</option>
+						<option value="pathology_path_report_type_lists">Pathology Type of Report</option>
+						<option value="pathology_morphology_lists">Morphology</option>
+						<option value="pathology_tissue_tumour_stage_lists">T Staging</option>
+						<option value="pathology_node_stage_lists">N Staging</option>
+						<option value="pathology_metastasis_stage_lists">M Staging</option>
+						<option value="pathology_tumour_stage_lists">Tumour Stage</option>
+						<option value="pathology_tumour_grade_lists">Tumour Grade</option>
+						<option value="patient_cancer_treatment_name_lists">Treatment Type</option>
+						<option value="ovary_stage_classification_lists">Pathology Stage Classification</option>
+						<option value="diagnosis_name_lists">Type of Diseases</option>
+					</optgroup>
+					<optgroup label="Screenings">
+					</optgroup>
+					<optgroup label="Mutation">
+					</optgroup>
+					<optgroup label="Lifestyle Factors">
+					</optgroup>
 					</select>
 				</td>
 			</tr>
