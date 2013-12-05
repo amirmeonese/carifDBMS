5e983030f068c28164e253390fcfa55aa94dbe12
famir <farizaamir@apurbatech.com>
2013-12-05 13:05:45 +0800

    Added link for favicon

--- application/views/templates/record_home_template.php
+++ application/views/templates/record_home_template.php
@@ -10,7 +10,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta name="description" content="">
         <meta name="author" content="">
-
+		<link href="<?php echo base_url(); ?>img/favicon.ico" rel="icon" type="image/x-icon" /> 
         <link href="<?php echo base_url(); ?>css/bootstrap.css" rel="stylesheet">
         <link href="<?php echo base_url(); ?>css/bootstrap-responsive.min.css" rel="stylesheet">
 
